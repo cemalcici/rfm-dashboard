8eeb67c28a691f1a6289e81e702837685de24d0f
cemalcici <cicicemal@gmail.com>
2021-02-21 19:41:35 +0300

    completed data preprations

--- rfm_analysis.py
+++ rfm_analysis.py
@@ -7,5 +7,30 @@ import pandas as pd
 import datetime as dt
 import pickle
 
+###########################################
 # Veri Setinin Yüklenmesi
+###########################################
 df = pd.read_csv("online-retail-germany.csv", sep=";")
+
+###########################################
+# Veri Ön İşleme
+###########################################
+
+# InvoiceDate dönüşümü
+df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+
+
+# İadeleri çıkarma
+df = df[~(df["Invoice"].astype(str).str.contains("C"))]
+
+# Fatura numaraları alfanumerik olmayan faturaları çıkarma
+df = df[~(df["Invoice"].astype(str).str.isalpha())]
+
+# Eksik değerlere ait gözlemleri silme
+df.dropna(inplace=True)
+
+# Toplam ücreti hesaplama
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+# Customer ID dönüşümü
+df["Customer ID"] = df["Customer ID"].astype(int).astype(str)
